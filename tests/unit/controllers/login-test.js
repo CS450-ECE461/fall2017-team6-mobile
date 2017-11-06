c64f09ab6b1098342ed71c2e7e7312ae2fb74129
Chaldric <aldrich.chris@comcast.net>
2017-11-06 00:53:59 -0500

    Fixed the Login controller file.

--- tests/unit/controllers/login-test.js
+++ tests/unit/controllers/login-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('controller:logins', 'Unit | Controller | logins', {
+moduleFor('controller:login', 'Unit | Controller | login', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
